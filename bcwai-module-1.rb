dc0808083d366a763f906d892dd1f863d4296e66
Chandima Dutton <chandimad@amphora-research.com>
2025-05-12 15:17:37 +0000

    Module 1 initial progress

---
+++ bcwai-module-1.rb
@@ -0,0 +1,428 @@
+require 'bcf/flightplans'
+require_relative './common_blocks'
+require_relative './common_resources'
+
+MODULE_1 = BCF::FlightPlans::ConventionalFlightPlan.build do
+  module_title "Human-AI Interaction"
+  module_number 1
+
+  learning_outcomes <<~MD
+    Learners will be able to:
+
+    -	Explain Human-AI interaction in the context of conversations with AIs
+
+    Suggested learning outcomes for further trainings/interventions
+
+    - Discuss the different AI models and their capabilities
+  MD
+
+  demo <<~MD
+    tbc
+  MD
+
+  block(BCF::FlightPlans::CommonBlocks::PRE_FLIGHT)
+
+  block(BCF::FlightPlans::CommonBlocks::GREETING)
+
+  instruction_starts
+
+  block(name: "Welcome to the course", lead_by: :fx1) do
+    length 4
+
+    resources do
+      flipchart(
+        :flip_1,
+        comment: "",
+        description: "Course outline and agenda (pre-prepared)",
+        scribed_by: :fx1
+      )
+    end
+
+    facilitator do
+      instruction <<~MD
+        Welcome people and introduce facilitator(s), producer and any observers and briefly explain their roles.
+
+        Explain that the course attendees are invited to learn any of these roles too, by working with the Foundation.
+      MD
+
+      spoken "There are some contact details going in the chat if you need to get in touch." 
+        
+      # TODO: "These are also in the Course Handbook"
+
+      spoken "Does everyone have experience with Zoom?"
+
+      instruction "Adjust response as necessary."
+
+      spoken "You can use translated captions to follow us more easily. Turn this on from the options at the bottom of your Zoom window. You may see a box pop-up on your screen to enter your spoken language if someone turns this on."
+
+      spoken "The producer is on hand to help with any technical issues."
+
+      # TODO: "We will be using the flipcharts and chat facility during this course. You don't need to be able to read everything on the flipcharts, but you can pin my Zoom window if you'd like to see more."
+
+      spoken "Let's try the chat facility out:"
+
+      spoken_exact "Please let us know where you are in the world by typing in chat."
+
+      instruction "Delivery team type where they are in the chat."
+
+      # TODO: Spoken <<-MD Doesnt add a bullet point every new line however adds different styling to bullet points put in
+      spoken <<~MD
+
+        We've designed this course to be experiential. Because of that, we ask that you respect the confidentiality of what's shared in this group to allow everyone to fully engage with the learning.
+
+      MD
+
+      instruction "Go through the course overview on flip"
+
+      spoken <<~MD
+        This course introduces some key aspects of working with AIs. By the end of the course, you will have practical tools and strategies to prepare for and have better conversations with AIs.
+
+        The five topics we'll cover are Human-AI interaction, Goal-setting, Oversight, Feedback and Ethical Use of AIs.
+
+        Each module with be an hour long and we will stay on the Zoom call for 15 minutes after the session if you'd like to talk to us in depth.
+
+        Fieldwork will be sent out by email after each module. We need you to do this to consolidate your learning and to prepare for the next module. Please do reach out to us for help with this or any other questions during the week.
+
+        We will be using the Amphora Chat platform for this course which gives you access to a number of different AI models.
+
+        We'll also be sending out handouts for each module to support your learning.
+
+        Any questions about this?
+      MD
+
+      # TODO: Add the comments about the 1 hour and afterparty to the BC course.# TODO: Move the pattern of activities spoken to after the instruction in the BC course. 
+      
+      instruction "Facilitator quickly goes through the agenda points on flip"
+      spoken "We will be following the same pattern of activities each time."
+      spoken "Today, we are going to discuss Human-AI interaction. Being aware of the differences between humans and AI can help us have better conversations with AIs to steer them towards our goals."
+    end
+
+    producer do
+      instruction "Copy to chat when you hear facilitator mention contact details:"
+
+      chat <<~MD
+
+        email: help@betterconversations.foundation
+        phone: +44 118 234 9811
+        website: https://betterconversations.foundation
+
+      MD
+
+      instruction "Copy to chat when the facilitator mentions location"
+
+      chat <<~MD
+        - Where are you in the world (country, region, building/room)?
+      MD
+
+      instruction "Give example of where you are when facilitator mentions where they are."
+    end
+  end
+
+  block(BCF::FlightPlans::CommonBlocks::ANY_QUESTIONS)
+
+  block(name: "Check-in", lead_by: :fx2) do
+    length 4
+    section_comment "Gather comments in chat and pick out some examples"
+
+    facilitator do
+
+      spoken "Before we get into the content, let's check in to find out your level of comfort with using AIs."
+
+      spoken_exact <<~MD
+      
+        On a scale of 0-10, where:
+
+        - 0 is extremely uncomfortable
+        - 10 is very comfortable
+        
+        Please put into the chat how you comfortable you are with using AIs.
+
+      MD
+
+      instruction <<~MD
+
+        Accept whatever is put in chat. If people are:
+
+        0-4: ask them to let us know whether they need us to slow down as we go and explain it differently.
+
+        8-10: ask them to notice what is being raised, to understand some of the issues people are thinking about in relation to implementing AIs in their work.
+
+      MD
+
+    end
+
+    producer do
+      instruction "Copy to chat when facilitator mentions scale for check-in"
+
+      chat <<~MD
+
+        - How comfortable are you with using AIs?
+
+        - Please give us a number between 0 (extremely uncomfortable) and 10 (very comfortable)
+
+      MD
+
+      instruction "Take note of the numbers to decide BOR participants [tbc whether to avoid people with low scores in the same room]"
+    end
+
+  end
+
+  block(name: "Motivation for being on the course", lead_by: :fx2) do
+    length 5
+    section_comment "Gather comments in chat and pick out some examples"
+
+    facilitator do
+      spoken "Let's hear from you all in the chat..."
+
+      spoken_exact <<~MD
+        - What brings you to be on this course?
+        - What are you hoping to get out of this course?
+      MD
+
+      instruction "Pick out 1-2 examples from chat and ask people to expand on them. Acknowledge what they say."
+
+      handover "for Amphora Chat platform"
+        
+    end
+
+    producer do
+
+      chat <<~MD
+        - What brings you to be on this course?
+        - What are you hoping to get out of this course?
+      MD
+
+    end
+
+  end
+
+  block(name: "Amphora Chat platform", lead_by: :fx1) do
+    length 5
+    resources do
+      flipchart(
+        :flip_2,
+        comment: "Use flip to help explain the platform",
+        description: "tbc",
+        scribed_by: :fx1
+      )
+    end
+
+    # TODO: When Handbook goes on website, check talking about the space in the handbook is still relevant in all flight plans
+  
+    facilitator do
+      spoken "We are going to demo the Amphora Chat platform now."
+
+      instruction "One of the delivery team demos the platform"
+
+      instruction <<~MD
+        - Show how to navigate the platform
+        - Explain this is set up as a basic model for this session
+        - Explain that we will be using the platform for the rest of the course including for doing the fieldwork
+        - Confirm we can see their messages on the platform so they should not enter any sensitive or confidential information on the platform
+      MD
+    end
+
+    producer do
+      instruction "Prepare BORs in 2/3s for 5 minutes if not already done"
+    end
+
+  end
+
+
+  block(name: "Breakout 1", lead_by: :fx2) do
+    length 6
+    section_comment "(1 min briefing)"
+
+    resources do
+      breakout_room(:bor_1, default_duration: 5, notify_halfway: true, halfway_message: true)
+    end
+
+    facilitator do
+      spoken "We are going to send you into breakout rooms for 5 minutes."
+      spoken_exact <<~MD
+        - Try typing this prompt into the Amphora Chat platform:
+        - How do I boil an egg?
+        - You can also try out other prompts to get used to the platform.
+        - If you run into any issues message us in the Zoom chat and we can help you.
+      MD
+
+      spoken "Any questions before we send you in?"
+    end
+
+    producer do
+      instruction "Copy to chat when facilitator mentions this: "
+
+      chat <<~MD
+        - Try typing this prompt into the Amphora Chat platform: 
+        
+        *How do I boil an egg?*
+
+        You can also try out other prompts to get used to the platform. If you run into any issues message us in the Zoom chat and we can help you.
+      MD
+
+      instruction "When the facilitator has finished talking, tell the group you are going to send them into BORs for 5 minutes"
+
+      send_into_bor(:bor_1)
+    end
+  end
+
+  block(name: "Unpack Breakout 1 ", lead_by: :fx2) do
+    length 5
+    resources do
+      flipchart(
+        :flip_2,
+        comment: "Summarise responses on flip, using people's words",
+        description: "Observations on Amphora Chat platform",
+        scribed_by: :fx1
+      )
+    end
+
+    facilitator do
+      spoken "Welcome back"
+
+      spoken_exact <<~MD
+        - What did you notice about the Amphora Chat platform?
+        - Any questions about the platform?
+      MD
+
+      instruction "Facilitate brief discussion suggesting people can also put their answers in the chat. Aim for 2-3 examples, with one from the chat."
+    end
+
+    producer do
+      instruction "Copy to chat:"
+
+      chat <<~MD
+        - What did you notice about the Amphora Chat platform?
+        - Any questions about the platform?
+      MD
+
+      instruction "Prepare BORs in same 2/3s for 4 minutes "
+
+    end
+
+  end
+
+  block(name: "Breakout 2", lead_by: :fx2) do
+    length 5
+    section_comment "(1 min briefing)"
+
+    resources do
+      breakout_room(:bor_2, default_duration: 4, notify_halfway: true, halfway_message: true)
+    end
+
+    facilitator do
+      spoken "We are going to do a bit more with the AI model we just used. We are going to explore what it can and can't do and introduce more context into our AI conversation."
+      
+      spoken_exact <<~MD
+
+      - Enter these prompts this time, and note the responses between each prompt
+      - 
+      spoken "This time, come back with examples of being in amber/yellow states that you are happy to share in the wider group. You’ll have 4 minutes this time."
+      spoken "We’ll put the question in the chat again…"
+    end
+
+    producer do
+      instruction "Copy to chat"
+
+      chat "What might you be thinking or feeling when you’re in an amber/yellow state?"
+
+      instruction "When the facilitator has finished talking, tell the group you are going to send them into BORs for 4 minutes"
+      send_into_bor :bor_2
+    end
+  end
+
+  block(name: "Unpack Breakout 2", lead_by: :fx2) do
+    length 5
+    resources do
+      flipchart(
+        :flip_2,
+        comment: "Add to flip in the amber/yellow state section for up to 3 people",
+        description: "Findings on amber/yellow state",
+        scribed_by: :fx1
+      )
+    end
+
+    facilitator do
+      spoken "Now let’s get 2-3 examples of amber/yellow state"
+
+      spoken_exact "What did you notice about being in an amber/yellow state?"
+
+      instruction <<~MD
+        Facilitate brief discussion, helping people to say what they are thinking or feeling in an amber/yellow state. Suggest people can also put their answers in the chat.
+
+        If time allows, focus in on the transition between amber/yellow and other states and the reasons for that.
+      MD
+
+      handover "for reflections, fieldwork and close"
+    end
+
+    producer do
+      instruction "Copy to chat: "
+
+      chat "What did you notice about being in an amber/yellow state?"
+    end
+  end
+
+  block(name: "Reflect on learning in this session", lead_by: :fx1) do
+    length 5
+    section_comment "Gather comments in chat and pick out some examples"
+
+    facilitator do
+      spoken "You’ve had some conversations and learned a model to describe state. This is a key ingredient in having Better Conversations."
+      spoken "Take a moment now to reflect and put your thoughts in the chat. Here are some questions to guide your thinking:"
+
+      spoken_exact "Thinking about conversations that have and haven’t gone well and what you know now about state, what are you noticing?"
+      spoken_exact "What difference does this make?"
+      spoken_exact "Is there something you may do differently now you know this?"
+
+      instruction "Use chat to gather comments. If time permits, invite examples from 1-2 people."
+
+      spoken "We are almost at the end of this first module. There is space in the Course Handbook to capture any more thoughts you might have from today. "
+    end
+
+    producer do
+      instruction "Copy to chat as facilitator mentions fieldwork: "
+
+      chat <<~MD
+        - Thinking about conversations that have and haven’t gone well and what you know now about state
+
+        - What are you noticing?
+        - What difference does this make?
+        - Is there something you may do differently now you know this?
+      MD
+    end
+
+  end
+
+  # TODO: Check this can use the common fieldwork block and array - wording is different at start
+  block(name: "Fieldwork", lead_by: :fx1) do
+    length 2
+
+    facilitator do
+      spoken "We have fieldwork that is optional but very useful to consolidate what we cover in each module. We will send this out by email."
+      spoken "The suggested fieldwork for this module is to:"
+      spoken_exact "Notice your state, and how and when it changes for you."
+      spoken_exact "Try mapping it to the Traffic Light Model."
+      spoken_exact "What’s your state like, before and after your conversations?"
+    end
+
+    producer do
+      instruction "Copy to chat as facilitator mentions fieldwork: "
+
+      chat <<~MD
+        - Fieldwork:
+
+        - Notice your state, and how and when it changes for you.
+        - Try mapping it to the Traffic Light Model.
+        - What’s your state like, before and after your conversations?
+      MD
+    end
+  end
+
+  block(BCF::FlightPlans::CommonBlocks::Closing.new(:fx1, "Assumptions", length: 1))
+
+  instruction_ends
+
+  block(BCF::FlightPlans::CommonBlocks::SPONSOR_CLOSE)
+  block(BCF::FlightPlans::CommonBlocks::SPONSOR_DEBRIEF)
+end
