45b8eee1221e269e584d047b7c10172718351abc
Chandima Dutton <chandimad@amphora-research.com>
2025-05-20 21:09:11 +0000

    remove version for config as no longer needed.

--- source/shared/course_config.rb
+++ source/shared/course_config.rb
@@ -1,11 +1,10 @@
 module CourseConfig
   class Config
-    attr_reader :identifier, :title, :version
+    attr_reader :identifier, :title
 
     def initialize(identifier:, title:)
       @identifier = identifier
       @title = title
-      @version = ENV['COURSE_VERSION'] || raise("Version must be set via COURSE_VERSION")
     end
   end
 end 
